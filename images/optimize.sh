14aaa1dc20d7064faac000b8d2308948425d99b1
ryanholbrook <44940142+ryanholbrook@users.noreply.github.com>
2020-01-29 00:23:28 -0600

    Publish.

--- images/optimize.sh
+++ images/optimize.sh
@@ -2,7 +2,8 @@
 
 for i in *.gif; do
     ffmpeg -n -i "$i" -c vp9 -b:v 0 -crf 41 "${i%.*}.webm";
-    ffmpeg -n -i "$i" -c vp9 -b:v 0 -crf 41 "${i%.*}.mp4";
+    ffmpeg -n -i "$i" -c h264 -b:v 0 -crf 41 "${i%.*}.mp4";
+    
 done
 
 ## https://css-ig.net/pingo
